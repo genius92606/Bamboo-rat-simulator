1292ababc0722f35f7bc7c504ae1ec2b933c32d0
genius92606 <martin33156@gmail.com>
2018-10-28 16:04:28 +0800

    push

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ $("#key-features").click(function(){
   $('html,body').animate({
     scrollTop: $(".second-text").offset().top},'slow');
   });
-$("key-features").mouseenter(function(){
+$("#key-features").mouseenter(function(){
   $(this).css("cursor","Pointer");
   });
-$("key-features").mouseleave(function(){
+$("#key-features").mouseleave(function(){
   $(this).css("cursor","default");
   });
 
