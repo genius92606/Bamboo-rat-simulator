e0304c0fe4e7493fd51638b51ed3fd34a9f40d9a
genius92606 <martin33156@gmail.com>
2018-10-28 15:57:44 +0800

    click

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 $("#key-features").click(function(){
   $('html,body').animate({
     scrollTop: $(".second-text").offset().top},'slow');
-  alert("fuck");
   });
+$("key-features").mouseenter(function(){
+  $(this).css("cursor","Pointer");
+  });
+$("key-features").mouseleave(function(){
+  $(this).css("cursor","default");
+  });
+
