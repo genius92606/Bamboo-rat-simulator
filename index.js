ddcccc5fcf2401bdfc2b7438979d1d8a14106bd0
genius92606 <martin33156@gmail.com>
2018-10-28 16:10:02 +0800

    haha

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ $("#key-features").click(function(){
   $('html,body').animate({
     scrollTop: $(".second-text").offset().top},'slow');
   });
-$("#key-features").mouseenter(function(){
-  $(this).css("cursor","Pointer");
-  });
-$("#key-features").mouseleave(function(){
-  $(this).css("cursor","auto");
-  });
 
