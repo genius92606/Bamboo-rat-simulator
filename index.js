6ee6d93954a8c896047e690bfb6342bc2a5da425
NekoSaiKou <ricky0711.yang@gmail.com>
2018-11-04 22:23:09 +0800

    Modify about us

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
 $("#key-features").click(function(){
-  $('html,body').animate({
-    scrollTop: 1150 },'slow');
-  });
+  $('html,body').animate({scrollTop:$('.page2').height()},'slow');});
 $("#members").click(function(){
-  $('html,body').animate({
-    scrollTop: 1860 },'slow');
-  });
+  $('html,body').animate({scrollTop:$('.page2').height() + $('.page3').height()},'slow');});
 $("#feedback").click(function(){
-  $('html,body').animate({
-    scrollTop: 2600 },'slow');
-  });
+  $('html,body').animate({scrollTop: 2600 },'slow');});
 
-$("#circle-one").click(function(){
-  $('html,body').animate({
-    scrollTop: 0},'slow');
-  });
-$("#circle-two").click(function(){
-  $('html,body').animate({scrollTop: 1150},'slow');
-  });
-$("#circle-three").click(function(){
-  $('html,body').animate({scrollTop: 1860},'slow');
-  });
-$("#circle-four").click(function(){
-  $('html,body').animate({scrollTop: 2600},'slow');
-  });
+$("#circle-one").click(function(){$('html,body').animate({scrollTop: 0},'slow');});
+$("#circle-two").click(function(){$('html,body').animate({scrollTop: $('.page2').height()},'slow');});
+$("#circle-three").click(function(){$('html,body').animate({scrollTop: $('.page2').height() + $('.page3').height()},'slow');});
+$("#circle-four").click(function(){$('html,body').animate({scrollTop: 2600},'slow');});
 $(window).scroll(function(){
   var scrollVal=$(this).scrollTop();
-  if(scrollVal<=600){
+  if(scrollVal<=50){
     $("#circle-one").css("content","url(picture/black-circle.png)");
     $("#circle-two").css("content","url(picture/black-side.png)");
     $("#circle-three").css("content","url(picture/black-side.png)");
     $("#circle-four").css("content","url(picture/black-side.png)");
   }
-  if(scrollVal>=1150&&scrollVal<1500){
+  if(scrollVal>=$('.page2').height()-20&& scrollVal< $('.page2').height()+ 20){
     $("#circle-one").css("content","url(picture/black-side.png)");
     $("#circle-two").css("content","url(picture/black-circle.png)");
     $("#circle-three").css("content","url(picture/black-side.png)");
     $("#circle-four").css("content","url(picture/black-side.png)");
   }
-  if(scrollVal>=1860&&scrollVal<2300){
+  if(scrollVal>=$('.page2').height() + $('.page3').height()-20 && scrollVal< $('.page2').height() + $('.page3').height()+20){
     $("#circle-one").css("content","url(picture/black-side.png)");
     $("#circle-two").css("content","url(picture/black-side.png)");
     $("#circle-three").css("content","url(picture/black-circle.png)");
